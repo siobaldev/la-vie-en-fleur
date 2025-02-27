b526905d0abf25a6682d3c215b6ffe42723c4d58
br0wnrecluse <sblmnrd@gmail.com>
2025-02-28 06:29:56 +0800

    restore tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["var(--font-monggoland)"],
+        serif: ["var(--font-ptSerif)"],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
