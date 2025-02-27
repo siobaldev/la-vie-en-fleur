88d893a83beaeb00eb46c727458134749d3a7f90
br0wnrecluse <sblmnrd@gmail.com>
2025-02-27 23:32:24 +0800

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -52,6 +51,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
       <body
         className={`${monggoland.variable} ${ptSerif.variable} font-serif antialiased`}
       >
